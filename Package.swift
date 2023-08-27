37df41f4b789dc3ce8d6ed46e1aa249486084877
Paavo Becker <pEEcor@xcor.org>
2023-08-27 10:28:39 +0200

    Rename package and targets

--- Package.swift
+++ Package.swift
@@ -4,24 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-combine-extension",
+    name: "swift-combine-extensions",
     platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CombineExtension",
-            targets: ["CombineExtension"]
+            name: "CombineExtensions",
+            targets: ["CombineExtensions"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CombineExtension"
+            name: "CombineExtensions"
         ),
         .testTarget(
-            name: "CombineExtensionTests",
-            dependencies: ["CombineExtension"]
+            name: "CombineExtensionsTests",
+            dependencies: ["CombineExtensions"]
         ),
     ]
 )
