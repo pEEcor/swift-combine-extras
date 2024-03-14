9678f2c941f0144b399debbc51c4cf51294d0d17
Paavo Becker <paavo.becker@xcor.org>
2024-03-14 21:19:45 +0100

    Add tests for Future

--- Sources/CombineExtras/Extensions/Future.swift
+++ Sources/CombineExtras/Extensions/Future.swift
@@ -9,6 +9,31 @@ import Foundation
 import Combine
 import ConcurrencyExtras
 
+extension Future where Failure == Never {
+    /// Creates a Future that runs asynchronous work.
+    ///
+    /// The operation is started immediately when creating the future. If this is not desired,
+    /// consider wrapping the future into a `Deferred` publisher.
+    ///
+    /// - Parameter operation: The operation that should be executed.
+    @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    public convenience init(
+        _ operation: @Sendable @escaping () async -> Output
+    ) where Output: Sendable {
+        self.init { promise in
+            // The unchecked sendable promise can be consideres save to send since the promise is
+            // only ever called once.
+            let sendablePromise = UncheckedSendable(promise)
+            
+            // Start the task.
+            Task {
+                let output = await operation()
+                sendablePromise.value(.success(output))
+            }
+        }
+    }
+}
+
 extension Future where Failure == Error {
     /// Creates a Future that runs asynchronous work.
     ///
@@ -18,7 +43,7 @@ extension Future where Failure == Error {
     /// - Parameter operation: The operation that should be executed.
     @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
     public convenience init(
-        operation: @Sendable @escaping () async throws -> Output
+        _ operation: @Sendable @escaping () async throws -> Output
     ) where Output: Sendable {
         self.init { promise in
             // The unchecked sendable promise can be consideres save to send since the promise is
--- Tests/CombineExtrasTests/FutureTests.swift
+++ Tests/CombineExtrasTests/FutureTests.swift
@@ -36,4 +36,100 @@ final class FutureTests: XCTestCase {
 
         cancellable.cancel()
     }
+    
+    func testInit_shouldRunAsyncOperation() async throws {
+        await withMainSerialExecutor {
+            // Make an expectation that can be fulfilled when the sut publishes.
+            let expectation = expectation(description: "published")
+
+            // GIVEN
+            // The async future starts its operation immediately on creation. However it cannot run
+            // until the test task yields execution.
+            let sut: Future<Int, Never> = Future {
+                return 42
+            }
+
+            // Create subscription. At this state, the operation did not run yet, since the test
+            // task did not yield. Therefore the subsription is established before the publisher
+            // could run and publish anything.
+            let cancellable = sut.sink { output in
+                expectation.fulfill()
+            }
+
+            await fulfillment(of: [expectation], timeout: 3)
+
+            // Cleanup
+            cancellable.cancel()
+        }
+    }
+    
+    func testInit_shouldEmitOutput_whenOperationSucceeds() async throws {
+        await withMainSerialExecutor {
+            // Make an expectation that can be fulfilled when the sut publishes.
+            let expectation = expectation(description: "published")
+
+            // GIVEN
+            // The async future starts its operation immediately on creation. However it cannot run
+            // until the test task yields execution.
+            let sut: Future<Int, any Error> = Future {
+                return 42
+            }
+
+            // Create subscription. At this state, the operation did not run yet, since the test
+            // task did not yield. Therefore the subsription is established before the publisher
+            // could run and publish anything.
+            let cancellable = sut.sink(
+                receiveCompletion: { completion in
+                    guard case .finished = completion else {
+                        XCTFail("Expected finished")
+                        return
+                    }
+                    
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+            
+            await fulfillment(of: [expectation], timeout: 3)
+
+            // Cleanup
+            cancellable.cancel()
+        }
+    }
+    
+    func testInit_shouldEmitError_whenOperationFails() async throws {
+        await withMainSerialExecutor {
+            // Make an expectation that can be fulfilled when the sut publishes.
+            let expectation = expectation(description: "published")
+            
+            struct Failure: Error, Equatable {}
+
+            // GIVEN
+            // The async future starts its operation immediately on creation. However it cannot run
+            // until the test task yields execution.
+            let sut: Future<Int, any Error> = Future {
+                throw Failure()
+            }
+
+            // Create subscription. At this state, the operation did not run yet, since the test
+            // task did not yield. Therefore the subsription is established before the publisher
+            // could run and publish anything.
+            let cancellable = sut.sink(
+                receiveCompletion: { completion in
+                    guard case let .failure(failure) = completion else {
+                        XCTFail("Expected finished")
+                        return
+                    }
+                    XCTAssertEqual(failure as? Failure, Failure())
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+            
+            await fulfillment(of: [expectation], timeout: 3)
+
+            // Cleanup
+            cancellable.cancel()
+        }
+    }
 }
