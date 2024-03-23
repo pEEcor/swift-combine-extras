3d8b7eb400ad4ca0c012d32bec1ec3b283078965
Paavo Becker <paavo.becker@xcor.org>
2024-03-23 10:43:14 +0100

    Update Publisher+Stream implementation, add tests for Publisher+Stream

--- Sources/CombineExtras/Extensions/Publisher+Stream.swift
+++ Sources/CombineExtras/Extensions/Publisher+Stream.swift
@@ -10,68 +10,20 @@ import ConcurrencyExtras
 extension Publisher where Failure == Never {
     /// Creates an AsyncStream from a publisher that does not fail.
     ///
-    /// > Note: The observation of the publisher gets cancelled once the AsyncStream gets cancelled.
-    ///
-    /// - Parameter bufferingPolicy: The buffering policy for the AsyncStream.
     /// - Returns: The stream
-    @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-    public func asyncStream(
-        bufferingPolicy: AsyncStream<Output>.Continuation.BufferingPolicy = .unbounded
-    ) -> AsyncStream<Output> {
-        AsyncStream<Output>(bufferingPolicy: bufferingPolicy) { continuation in
-            // Access to the cancellable is not required here. Just the reference is needed to
-            // keep the subscription alive.
-            let cancellable = UncheckedSendable(self.sink { continuation.yield($0) })
-
-            // Store the cancellable inside the onTermination closure. This will retain the
-            // cancellable until the continuation gets cancelled
-            continuation.onTermination = { _ in
-                _ = cancellable
-            }
-        }
+    @available(macOS 12.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    public func asyncStream() -> AsyncStream<Output> {
+        self.values.eraseToStream()
     }
 }
 
 extension Publisher {
-    /// A typealias to shorten the type definition of an `AsyncThrowingStream`.
-    public typealias AsyncThrowingStreamOf<Publisher> = AsyncThrowingStream<
-        Publisher.Output, Publisher.Failure
-    > where Publisher: Combine.Publisher
-
     // Creates an AsyncThrowingStream from a publisher that can fail.
     //
-    // > Note: The observation of the publisher gets cancelled once the AsyncThrowingStream gets
-    // cancelled.
-    //
-    // - Parameter bufferingPolicy: The buffering policy for the AsyncThrowingStream.
     // - Returns: The stream
-    @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    @available(macOS 12.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
     public func asyncThrowingStream(
-        bufferingPolicy: AsyncThrowingStreamOf<Self>.Continuation.BufferingPolicy = .unbounded
-    ) -> AsyncThrowingStream<Output, Failure> where Failure == Error {
-        AsyncThrowingStream(bufferingPolicy: bufferingPolicy) { continuation in
-            let cancellable = self.sink(
-                receiveCompletion: { completion in
-                    switch completion {
-                    case .finished:
-                        continuation.finish()
-                    case let .failure(error):
-                        continuation.finish(throwing: error)
-                    }
-                }, receiveValue: { output in
-                    continuation.yield(output)
-                }
-            )
-
-            // Access to the cancellable is not required here. Just the reference is needed to
-            // keep the subscription alive.
-            let sendableCancellable = UncheckedSendable(cancellable)
-
-            // Store the cancellable inside the onTermination closure. This will retain the
-            // cancellable until the continuation gets cancelled
-            continuation.onTermination = { _ in
-                _ = sendableCancellable
-            }
-        }
+    ) -> AsyncThrowingStream<Output, Error> {
+        self.values.eraseToThrowingStream()
     }
 }
---
+++ Tests/CombineExtrasTests/Extensions/Publisher+StreamTests.swift
@@ -0,0 +1,45 @@
+//
+//  Publisher+StreamTests.swift
+//
+//  Copyright © 2024 Paavo Becker.
+//
+
+import Combine
+import ConcurrencyExtras
+import XCTest
+
+final class Publisher_StreamTests: XCTestCase {
+    func testAsyncStream_shouldEmitValuesInStream() async throws {
+        var result: [Int] = []
+        
+        // GIVEn
+        let publisher = [1, 2, 3].publisher
+        
+        // WHEN
+        for await value in publisher.asyncStream() {
+            result.append(value)
+        }
+        
+        // THEN
+        XCTAssertEqual(result, [1, 2, 3])
+    }
+    
+    func testAsyncThrowingStream_shouldEmitValuesInStream() async throws {
+        struct Failure: Error, Equatable {}
+        var result: [Int] = []
+        
+        // GIVEn
+        let publisher: AnyPublisher<Int, Failure> = [1, 2, 3]
+            .publisher
+            .setFailureType(to: Failure.self)
+            .eraseToAnyPublisher()
+        
+        // WHEN
+        for try await value in publisher.asyncThrowingStream() {
+            result.append(value)
+        }
+        
+        // THEN
+        XCTAssertEqual(result, [1, 2, 3])
+    }
+}
